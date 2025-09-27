ac4471e9a4dfe349fdafca079858cab2e168e1dc
Mathias Toala <fetoala@outlook.com>
2025-09-26 19:18:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const {app, Browserwindow} = require('electron')
+
+const createWindow = () => {
+    const win = new Browserwindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('./src/index.html')
+}
+
+app.whenReady().then( () => {
+    createWindow()
+})
+
 console.log("Eternal luv bboy");
