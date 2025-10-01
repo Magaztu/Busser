847203fe05a1ad10ea115dd04d6fceda5b6795c8
Mathias Toala <fetoala@outlook.com>
2025-09-30 23:58:39 -0500

    Pre-alpha is done and ready to be compiled, I hope you like it lil dude, don't worry as I will continue to update this until it becomes the best version ever

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const {app, BrowserWindow} = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        frame: false,
+        // titleBarOverlay: true,
+        // titleBarStyle: 'hidden'
     })
 
     win.loadFile('./src/index.html')
